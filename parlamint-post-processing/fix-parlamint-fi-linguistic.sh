71714d686a13d5fb37ff346fa25457cd0709f535
Jouni Tuominen <jouni.tuominen@aalto.fi>
2022-12-23 14:43:49 +0200

    fix for TEI.ana componen file titles

--- parlamint-post-processing/fix-parlamint-fi-linguistic.sh
+++ parlamint-post-processing/fix-parlamint-fi-linguistic.sh
@@ -2,3 +2,5 @@ sed -i '' -E 's|(teiCorpus.+xml:id="ParlaMint-FI)"|\1.ana"|' Data/ParlaMint-FI/P
 sed -i '' -E 's|(TEI.+xml:id="ParlaMint-FI[^"\.]+)"|\1.ana"|' Data/ParlaMint-FI/ParlaMint-FI_*.ana.xml
 
 sed -i '' -E 's|(include.+href="ParlaMint-FI[^\.]+)\.xml"|\1.ana.xml"|' Data/ParlaMint-FI/ParlaMint-FI.ana.xml
+
+sed -i '' -E 's|(title[^>]+>[^<]+\[ParlaMint)|\1.ana|' Data/ParlaMint-FI/ParlaMint-FI_*.ana.xml
