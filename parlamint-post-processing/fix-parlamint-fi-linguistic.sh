610779d77e7f9377b9a43ef9e70cf4c1d3c9121c
Jouni Tuominen <jouni.tuominen@aalto.fi>
2022-12-23 13:16:55 +0200

    fix TEI.ana root file component include file names to TEI.ana ones

--- parlamint-post-processing/fix-parlamint-fi-linguistic.sh
+++ parlamint-post-processing/fix-parlamint-fi-linguistic.sh
@@ -1,2 +1,4 @@
 sed -i '' -E 's|(teiCorpus.+xml:id="ParlaMint-FI)"|\1.ana"|' Data/ParlaMint-FI/ParlaMint-FI.ana.xml
 sed -i '' -E 's|(TEI.+xml:id="ParlaMint-FI[^"\.]+)"|\1.ana"|' Data/ParlaMint-FI/ParlaMint-FI_*.ana.xml
+
+sed -i '' -E 's|(include.+href="ParlaMint-FI[^\.]+)\.xml"|\1.ana.xml"|' Data/ParlaMint-FI/ParlaMint-FI.ana.xml
