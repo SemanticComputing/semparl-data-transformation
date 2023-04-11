bc802bda9b6b5eb4f65cecca94c3d3b9cf2f5d01
Jouni Tuominen <jouni.tuominen@aalto.fi>
2023-04-11 17:38:10 +0300

    fixes for parlamint

--- parlamint-post-processing/fix-parlamint-fi-linguistic.sh
+++ parlamint-post-processing/fix-parlamint-fi-linguistic.sh
@@ -3,4 +3,6 @@ sed -i '' -E 's|(TEI.+xml:id="ParlaMint-FI[^"\.]+)"|\1.ana"|' Data/ParlaMint-FI/
 
 sed -i '' -E 's|(include.+href="ParlaMint-FI[^\.]+)\.xml"|\1.ana.xml"|' Data/ParlaMint-FI/ParlaMint-FI.ana.xml
 
+sed -i '' 's|\[ParlaMint SAMPLE\]|\[ParlaMint.ana SAMPLE\]|' Data/ParlaMint-FI/ParlaMint-FI.ana.xml
+
 sed -i '' -E 's|(title[^>]+>[^<]+\[ParlaMint)|\1.ana|' Data/ParlaMint-FI/ParlaMint-FI_*.ana.xml
