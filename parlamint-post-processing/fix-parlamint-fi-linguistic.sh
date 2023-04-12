5037d42ed1cce28d5a74fe6839a754a77b694123
Jouni Tuominen <jouni.tuominen@aalto.fi>
2023-04-12 16:53:41 +0300

    fix link target pointers

--- parlamint-post-processing/fix-parlamint-fi-linguistic.sh
+++ parlamint-post-processing/fix-parlamint-fi-linguistic.sh
@@ -6,3 +6,5 @@ sed -i '' -E 's|(include.+href="ParlaMint-FI[^\.]+)\.xml"|\1.ana.xml"|' Data/Par
 sed -i '' 's|\[ParlaMint SAMPLE\]|\[ParlaMint.ana SAMPLE\]|' Data/ParlaMint-FI/ParlaMint-FI.ana.xml
 
 sed -i '' -E 's|(title[^>]+>[^<]+\[ParlaMint)|\1.ana|' Data/ParlaMint-FI/ParlaMint-FI_*.ana.xml
+
+sed -i '' -E 's|target="([^" ]+) ([^" ]+)"|target="#\1 #\2"|g' Data/ParlaMint-FI/ParlaMint-FI_*.ana.xml
