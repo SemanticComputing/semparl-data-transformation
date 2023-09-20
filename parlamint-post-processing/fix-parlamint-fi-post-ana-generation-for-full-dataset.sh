f777e36bae2b79a331998d576a72e8eb6479a19d
Jouni Tuominen <jouni.tuominen@aalto.fi>
2023-09-20 20:21:57 +0300

    parlamint post-processing scripts: post-ana-generation: don't create unnecessary .TEST files

--- parlamint-post-processing/fix-parlamint-fi-post-ana-generation-for-full-dataset.sh
+++ parlamint-post-processing/fix-parlamint-fi-post-ana-generation-for-full-dataset.sh
@@ -3,5 +3,5 @@
 for FILE_ANA in $DIR/ParlaMint-FI.ana.xml $DIR/*[0-9].ana.xml; do
   EXTENT=`grep -E '<extent>.+</extent>' $FILE_ANA`
   FILE=`echo $FILE_ANA | sed -E 's/.{8}$//'`.xml
-  sed -i '' "s|^.*<extent/>.*$|$EXTENT|" $FILE > $FILE".TEST"
+  sed -i '' "s|^.*<extent/>.*$|$EXTENT|" $FILE
 done
