953353fae67c9f0c491437600bf3dd3d568234a6
Jouni Tuominen <jouni.tuominen@aalto.fi>
2023-09-22 17:15:16 +0300

    update <extent> contents only if we got extent information from ana.xml

--- parlamint-post-processing/fix-parlamint-fi-post-ana-generation.sh
+++ parlamint-post-processing/fix-parlamint-fi-post-ana-generation.sh
@@ -2,6 +2,8 @@
 
 for FILE_ANA in $DIR/ParlaMint-FI.ana.xml $DIR/*[0-9].ana.xml; do
   EXTENT=`grep -E '<extent>.+</extent>' $FILE_ANA`
-  FILE=`echo $FILE_ANA | sed -E 's/.{8}$//'`.xml
-  sed -i '' "s|^.*<extent/>.*$|$EXTENT|" $FILE
+  if [[ $EXTENT != "" ]] ; then
+    FILE=`echo $FILE_ANA | sed -E 's/.{8}$//'`.xml
+    sed -i '' "s|^.*<extent/>.*$|$EXTENT|" $FILE
+  fi
 done
