e46e7267df435faeb2b897440d60074d254d0820
Jouni Tuominen <jouni.tuominen@aalto.fi>
2023-09-22 18:06:36 +0300

    fix the invalid linguistic annotation of a specific word occurrence (the link/@target value was invalid)

--- parlamint-post-processing/fix-parlamint-fi-linguistic.sh
+++ parlamint-post-processing/fix-parlamint-fi-linguistic.sh
@@ -20,3 +20,7 @@ sed -i '' -E 's/"UPosTag=([^"]+)\|"/"UPosTag=\1"/g' $DIR/ParlaMint-FI_*.ana.xml
 
 sed -i '' 's/:ns0//g' $DIR/ParlaMint-FI*.ana.xml
 sed -i '' 's/ns0://g' $DIR/ParlaMint-FI*.ana.xml
+
+sed -i '' 's/<w xml:id="ParlaMint-FI_2017-12-08-ps-134\.seg232\.28\.761" lemma="Ukko\.fin" msd="UPosTag=SYM|Abbr=Yes|Case=Gen|Number=Sing">Ukko\.fi:n<\/w>/<w xml:id="ParlaMint-FI_2017-12-08-ps-134\.seg232\.28\.761" lemma="Ukko\.fi" msd="UPosTag=PROPN|Abbr=Yes|Case=Gen|Number=Sing">Ukko\.fi:<\/w>/' $DIR/ParlaMint-FI_2017-12-08-ps-134.ana.xml
+sed -i '' 's/<w xml:id="ParlaMint-FI_2017-12-08-ps-134\.seg232\.28\.762" lemma="" msd="UPosTag=NOUN|Abbr=Yes|Case=Gen|Number=Sing" join="right" \/>/<w xml:id="ParlaMint-FI_2017-12-08-ps-134\.seg232\.28\.762" lemma="n" msd="UPosTag=NOUN|Abbr=Yes|Case=Gen|Number=Sing" join="right">n<\/w>/' $DIR/ParlaMint-FI_2017-12-08-ps-134.ana.xml
+sed -i '' 's/<link ana="ud-syn:goeswith" target="#ParlaMint-FI_2017-12-08-ps-134\.seg232\.28\.761 #ParlaMint-FI_2017-12-08-ps-134\.seg232\.28\.761" \/>/<link ana="ud-syn:flat_name" target="#ParlaMint-FI_2017-12-08-ps-134\.seg232\.28\.761 #ParlaMint-FI_2017-12-08-ps-134\.seg232\.28\.762" \/>/' $DIR/ParlaMint-FI_2017-12-08-ps-134.ana.xml
